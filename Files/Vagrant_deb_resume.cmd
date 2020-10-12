ef386a0970cb85539a3d6b37f697c3f6ef338d04
murzilius <murzilius@gmail.com>
2020-10-13 02:10:42 +0300

    Almost_ready

---
+++ Files/Vagrant_deb_resume.cmd
@@ -0,0 +1,4 @@
+cd /
+cd c:\Epam_test\Vagrant\bin\
+vagrant resume 0df1739
+pause
---
+++ Files/Vagrant_deb_suspend.cmd
@@ -0,0 +1,5 @@
+cd /
+cd c:\Epam_test\Vagrant\bin\
+vagrant suspend 0df1739
+vagrant snapshot save 0df1739 --force
+pause
